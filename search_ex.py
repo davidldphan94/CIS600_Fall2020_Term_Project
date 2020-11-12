282af6ae9d488cd6e98093aee646d2f3bc474957
nickweier <56059956+nickweier@users.noreply.github.com>
2020-11-12 17:40:24 -0500

    Update search_ex.py

--- search_ex.py
+++ search_ex.py
@@ -1,10 +1,10 @@
 import twitter
 
 def login():
-  CONSUMER_KEY='aLQEDUQZrJg0y783OUjK0s0tD'
-  CONSUMER_SECRET='29demwjvUdpQ5SQTA6cq7QIBbnkFoDd2dCYmpfg5nLNwaybs5v'
-  OAUTH_TOKEN='1311720591922286597-FUJ4rBoPG3lTpiIxwUqliTdKRZ5EWt'
-  OAUTH_TOKEN_SECRET='aqeX2Dm5sLoDeIFh6dxGHqO43ilRkw6wnKc3Xdd8QwuyM'
+  CONSUMER_KEY=''
+  CONSUMER_SECRET=''
+  OAUTH_TOKEN=''
+  OAUTH_TOKEN_SECRET=''
   auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,CONSUMER_KEY, CONSUMER_SECRET)
   twitter_api = twitter.Twitter(auth=auth)
   return twitter_api
